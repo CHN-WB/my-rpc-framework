2abf37ba834f4b205efff38144f9ec22bcd4702f
wangbu <wangbu_scu@163.com>
2021-05-24 21:52:08 +0800

    客户端动态代理

---
+++ rpc-core/src/main/java/com/wb/rpc/transport/RpcClient.java
@@ -0,0 +1,40 @@
+package com.wb.rpc.transport;
+
+import com.wb.rpc.entity.RpcRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * @ClassName: RpcClient
+ * @Description: 远程调用的客户端
+ * @Author: wangb
+ * @Date: 2021/5/24 21:47
+ */
+public class RpcClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(RpcClient.class);
+
+    public Object sendRequest(RpcRequest rpcRequest, String host, int port) {
+        /**
+         * socket套接字实现 TCP 网络传输
+         * try()中一般放对资源的申请，若{}出现异常，()资源自动关闭
+         */
+        try(Socket socket = new Socket(host, port)) {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            objectOutputStream.writeObject(rpcRequest);
+            objectOutputStream.flush();
+            return objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            logger.error("调用时有错误发生: ", e);
+            return null;
+        }
+    }
+
+}
---
+++ rpc-core/src/main/java/com/wb/rpc/transport/RpcClientProxy.java
@@ -0,0 +1,47 @@
+package com.wb.rpc.transport;
+
+import com.wb.rpc.entity.RpcRequest;
+import com.wb.rpc.entity.RpcResponse;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @ClassName: RpcClientProxy
+ * @Description: 客户端动态代理对象
+ * @Author: wangb
+ * @Date: 2021/5/24 21:41
+ */
+public class RpcClientProxy implements InvocationHandler {
+
+    private String host;
+    private int port;
+
+    // 传递 host 和 port 来指明服务端的位置
+    public RpcClientProxy(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    // 抑制编译器产生警告信息
+    @SuppressWarnings("unchecked")
+    public <T> T getProxy(Class<T> clazz) {
+        // 创建代理对象
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[]{clazz}, this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 客户端向服务端传输的对象，Builder模式生成
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .interfaceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .parameters(args)
+                .paramTypes(method.getParameterTypes())
+                .build();
+        // 进行远程调用的客户端
+        RpcClient rpcClient = new RpcClient();
+        return ((RpcResponse) rpcClient.sendRequest(rpcRequest, host, port)).getData();
+    }
+}
